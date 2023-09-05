bb6bc1580d334671c261a0ee239df430ecba6386
Samer Mobarak <samermobarak888@gmail.com>
2023-09-05 18:55:03 -0400

    sending_data

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+function submitData(name, email) {
+    const formData = {
+      name: name,
+      email: email
+    };
+  
+    const configurationObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+    };
+  
+    return fetch("http://localhost:3000/users", configurationObject)
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        const id = data.id;
+        document.body.innerHTML = `<p>${id}</p>`;
+      })
+      .catch(error => {
+        document.body.innerHTML = `<p>${error.message}</p>`;
+      });
+  }
+  
